5417a6e490c9417b66270bc60125549a02d7c4d7
Manoel Menezes <manoel.menezes.jr@gmail.com>
2017-07-22 18:44:15 -0300

    a different approach to signal exception not yet considered acceptable

--- src/main/scala/chapter04/Exceptions.scala
+++ src/main/scala/chapter04/Exceptions.scala
@@ -31,8 +31,46 @@ object Exceptions {
     }
   }
 
+  /**
+    * Seq is the common interface of various linear sequence-like collections.
+    * The mean function is an example of what’s called a partial function: it’s not defined for some inputs.
+    */
+  def mean(xs: Seq[Double]): Double = {
+
+    if (xs.isEmpty)
+      throw new ArithmeticException("mean of empty list!")
+
+    xs.sum / xs.length
+  }
+
+  /**
+    * This makes mean into a total function, but it has drawbacks—it requires that immediate
+    * callers have direct knowledge of how to handle the undefined case and limits them to
+    * returning a Double. What if mean is called as part of a larger computation and we’d
+    * like to abort that computation if mean is undefined? Or perhaps we’d like to take some
+    * completely different branch in the larger computation in this case? Simply passing an
+    * onEmpty parameter doesn’t give us this freedom.
+    * We need a way to defer the decision of how to handle undefined cases so that they
+    * can be dealt with at the most appropriate level.
+    */
+  def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double = {
+    if (xs.isEmpty)
+      onEmpty
+    else
+      xs.sum / xs.length
+  }
+
   def main(args: Array[String]): Unit = {
     println(failingFn2(12))
+
+    val l = Seq[Double]()
+
+    val d = l.sum / l.length
+
+    println(d)
+
+    println(d + 1)
+
   }
 
 }
